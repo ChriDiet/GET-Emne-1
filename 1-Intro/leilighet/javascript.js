35c4d979477218efda3592df2cb645e5f9391bb2
Christina <ch.dietrichs@yahoo.no>
2025-01-23 15:27:43 +0100

    Add function for livingroom
    Added a funtction for entering the livingroom, i also changed some html code.

--- 1-Intro/leilighet/javascript.js
+++ 1-Intro/leilighet/javascript.js
@@ -1,33 +1,43 @@
 
 function outsideApartment() {
+   document.getElementById("apartment").classList.add("utenfor");
    document.getElementById("apartment").innerHTML = /*HTML*/
-   `<div>
+      `<div>
       <button onclick="enterHallway()" id="enter">Gå Inn</button>
       <p id="info">Du er utenfor en leilighet</p>
    </div>`;
-   }
-   outsideApartment();
+}
+outsideApartment();
 
-   function enterHallway() {
-      document.getElementById("apartment").classList.add("leilighet2");
-      document.getElementById("apartment").innerHTML = /*HTML*/
-      `<p id="info">Du er nå i gangen</p>
-      <div id="deg">✸</div>
+function enterHallway() {
+   document.getElementById("apartment").classList.add("leilighet2");
+   document.getElementById("apartment").innerHTML = /*HTML*/
+      `<div id="deg">Du er nå i gangen</div>
       <button id="gang" style="border:none;">Gang</button>
       <button onclick="enterLivingroom()" id="stue">Stue</button>
       <button onclick="enterBathroom()" id="bad">Bad</button>
-      <button onclick="enterKitchen()" id="kjøkken">Kjøkken</button>`;
-      }
+      <button onclick="enterKitchen()" id="kjøkken">Kjøkken</button>
+      <button onclick="outsideApartment()" id="utsiden">Gå ut</button>`;
+}
 
-      function enterKitchen() {
-         document.getElementById("apartment").classList.add("leilighet2");
-         document.getElementById("apartment").innerHTML = /*HTML*/
-         `<p id="info">Du er nå på kjøkkenet</p>
-         <div id="deg">✸</div>
+function enterKitchen() {
+   document.getElementById("apartment").classList.add("leilighet2");
+   document.getElementById("apartment").innerHTML = /*HTML*/
+      `<div id="deg">Du er nå på kjøkkenet</div>
          <button onclick="enterHallway()" id="gang">Gang</button>
          <button onclick="enterLivingroom()"id="stue">Stue</button>
          <button id="kjøkken" style="border:none;">Kjøkken</button>`;
-         }
-      outsideApartment();
+}
+
+function enterLivingroom() {
+   document.getElementById("apartment").classList.add("leilighet2");
+   document.getElementById("apartment").innerHTML = /*HTML*/
+      `<div id="deg">Du er nå i stua</div>
+      <button id="gang">Gang</button>
+      <button onclick="enterLivingroom()" style="border:none;" id="stue">Stue</button>
+      <button onclick="enterKitchen()" id="kjøkken">Kjøkken</button>`;
+}
+
+enterLivingroom();
 
 
