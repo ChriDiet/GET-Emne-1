0908150984ef53b9f9e68696d4eceb35a5062966
Christina <ch.dietrichs@yahoo.no>
2025-01-23 14:55:35 +0100

    Add function to enter hallway

---
+++ 1-Intro/leilighet/javascript.js
@@ -0,0 +1,25 @@
+
+function outsideApartment() {
+   document.getElementById("apartment").innerHTML = /*HTML*/
+   `<div>
+      <button onclick="enterHallway()" id="enter">Gå Inn</button>
+      <p id="info">Du er utenfor en leilighet</p>
+   </div>`;
+   }
+   outsideApartment();
+
+   function enterHallway() {
+      document.getElementById("apartment").classList.add("leilighet2");
+      document.getElementById("apartment").innerHTML = /*HTML*/
+      `<p id="info">Du er nå i gangen</p>
+      <div id="deg">✸</div>
+      <button id="gang" style="border:none;">Gang</button>
+      <button onclick="enterLivingroom()" id="stue">Stue</button>
+      <button onclick="enterBathroom()" id="bad">Bad</button>
+      <button onclick="enterKitchen()" id="kjøkken">Kjøkken</button>`;
+      }
+
+
+      outsideApartment();
+
+
