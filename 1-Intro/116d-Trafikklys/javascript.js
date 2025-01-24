9163a68f5764b57da24456cb96891fcaf6290f24
Christina <ch.dietrichs@yahoo.no>
2025-01-24 17:04:22 +0100

    Finalize trafficlight Deleted javascript and css files and moved everything into index file.

--- 1-Intro/116d-Trafikklys/javascript.js
+++
@@ -1,8 +0,0 @@
-function showRed() {
-   document.getElementById("yellowLight").style.backgroundColor="grey";
-   document.getElementById("greenLight").style.backgroundColor="grey";
-   document
-}
-
-showRed();
-
