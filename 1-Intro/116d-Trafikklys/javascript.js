19c73a19a734900b228a3e0040ed7598759b5573
Christina <ch.dietrichs@yahoo.no>
2025-01-24 14:49:39 +0100

    Make function for redlight

---
+++ 1-Intro/116d-Trafikklys/javascript.js
@@ -0,0 +1,8 @@
+function showRed() {
+   document.getElementById("yellowLight").style.backgroundColor="grey";
+   document.getElementById("greenLight").style.backgroundColor="grey";
+   document
+}
+
+showRed();
+
