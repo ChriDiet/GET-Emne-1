9ac2451c5d5a19b6485cdcee955c6c207cdba423
Christina <ch.dietrichs@yahoo.no>
2025-03-21 19:30:06 +0100

    Add function for changing result rows

--- 4-Løkker-Lister/153a-b-numberPaging/view.js
+++ 4-Løkker-Lister/153a-b-numberPaging/view.js
@@ -1,9 +1,41 @@
-function updateView() {
+   function updateView() {
    document.getElementById('app').innerHTML = /*HTML*/ `
-   <
-      ${createNumbersListHtml()}
-      ${createButtonsHtml()}
+      ${createNumbersListHtml()}<br><br>
+      ${createButtonsHtml()}<br><br>
+      <div> 
+         Antall treff 
+         <select onchange="setPageSize(value)" name="pageView" id="pageView" value="value">
+            <option value=10 ${selectedPageSize == 10 ? 'selected' : ''}>10</option>
+            <option value=20 ${selectedPageSize == 20 ? 'selected' : ''}>20</option>
+            <option value=50 ${selectedPageSize == 50 ? 'selected' : ''}>50</option>
+         </select>
+      </div>
    `;
+}
+
+function createResultSelectorHtml() {
+   let resultQty = [10,20,50];
+   
+}
+
+function setPageSize(size) {
+   pageSize = size;
+   selectedPageSize = size;
+   skipAndTake(pageIndex)
+   updateView()
+}
+function skipAndTake(pageIndex) {
+   let skipCount = pageIndex * pageSize;
+   let takecount = pageSize;
+   numbersToDisplay = [];
+   for (let number of numbers) {
+      skipCount--;
+      if (skipCount >= 0) continue;
+      if (takecount <= 0) break;
+      takecount--;
+      numbersToDisplay.push(number);
+   }
+   updateView()
 }
 
 function createButtonsHtml() {
